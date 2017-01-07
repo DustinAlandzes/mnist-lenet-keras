c4e5d9c6cd92b9d8b50cc440700ce8799def839f
f00 <f00@f00s-MacBook-Pro.local>
2017-01-07 00:56:00 -0600

    Added requirements.txt and removed cmd line arguments

--- mnist.py
+++ mnist.py
@@ -1,31 +1,14 @@
-# USAGE
-# python lenet_mnist.py --save-model 1 --weights output/lenet_weights.hdf5
-# python lenet_mnist.py --load-model 1 --weights output/lenet_weights.hdf5
-
-# import the necessary packages
 from lenet import LeNet
 from sklearn.cross_validation import train_test_split
 from sklearn import datasets
 from keras.optimizers import SGD
 from keras.utils import np_utils
 import numpy as np
-import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--save-model", type=int, default=-1,
-	help="(optional) whether or not model should be saved to disk")
-ap.add_argument("-l", "--load-model", type=int, default=-1,
-	help="(optional) whether or not pre-trained model should be loaded")
-ap.add_argument("-w", "--weights", type=str,
-	help="(optional) path to weights file")
-args = vars(ap.parse_args())
+weightsPath = "weights/lenet_weights.hdf5"
 
-# grab the MNIST dataset (if this is your first time running this
-# script, the download may take a minute -- the 55MB MNIST dataset
-# will be downloaded)
-print("[INFO] downloading MNIST...")
+print("downloading MNIST...")
 dataset = datasets.fetch_mldata("MNIST Original")
 
 # reshape the MNIST dataset from a flat list of 784-dim vectors, to
@@ -47,13 +30,12 @@ testLabels = np_utils.to_categorical(testLabels, 10)
 print("[INFO] compiling model...")
 opt = SGD(lr=0.01)
 model = LeNet.build(width=28, height=28, depth=1, classes=10,
-	weightsPath=args["weights"] if args["load_model"] > 0 else None)
+	weightsPath=weightsPath)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# only train and evaluate the model if we *are not* loading a
-# pre-existing model
-if args["load_model"] < 0:
+# if no weights specified train the model
+if weightsPath is None:
 	print("[INFO] training...")
 	model.fit(trainData, trainLabels, batch_size=128, nb_epoch=20,
 		verbose=1)
@@ -64,10 +46,8 @@ if args["load_model"] < 0:
 		batch_size=128, verbose=1)
 	print("[INFO] accuracy: {:.2f}%".format(accuracy * 100))
 
-# check to see if the model should be saved to file
-if args["save_model"] > 0:
 	print("[INFO] dumping weights to file...")
-	model.save_weights(args["weights"], overwrite=True)
+	model.save_weights(weightsPath, overwrite=True)
 
 # randomly select a few testing digits
 for i in np.random.choice(np.arange(0, len(testLabels)), size=(10,)):
@@ -75,8 +55,7 @@ for i in np.random.choice(np.arange(0, len(testLabels)), size=(10,)):
 	probs = model.predict(testData[np.newaxis, i])
 	prediction = probs.argmax(axis=1)
 
-	# resize the image from a 28 x 28 image to a 96 x 96 image so we
-	# can better see it
+	# resize the image from a 28 x 28 to 96 x 96
 	image = (testData[i][0] * 255).astype("uint8")
 	image = cv2.merge([image] * 3)
 	image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_LINEAR)
